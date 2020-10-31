1789965f5d10ab5f8c00ca713774fab5ad49314f
Marina Permiakova <marina.mk.glazov@gmail.com>
2020-10-31 11:22:41 +0400

    test correction 3

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -74,6 +74,6 @@ class MainKtTest {
                 lastTransaction = userLastTransaction,
                 previousTransaction = userPreviousTransaction
         )
-        assertEquals(10, result)
+        assertEquals(0, result)
     }
 }
